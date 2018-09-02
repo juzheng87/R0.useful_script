354d6ee85c0b2c870d013abe1dac06ca8d855b17
juzheng87 <zhengju2016@126.com>
2018-09-02 20:08:44 +0800

    Initial commit

---
+++ code/python/test.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python  
+# -*- coding:utf-8 -*-  
+__author__ = "Ju zheng"
+__version__ = "version 0.1"
+
+#HomePage: 
+#FileName: *.py  
+
+import os
+import sys
+import collections
+import re
+import argparse
+import glob
+import commands
+
+"""
+@version: ??
+@author: phpergao
+@license: Apache Licence 
+@contact: endoffight@gmail.com
+@site: 
+@software: PyCharm
+@file: test.py.py
+@time: 2018/9/2 0002 下午 19:58
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
